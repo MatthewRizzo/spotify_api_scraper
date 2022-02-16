a7fe47018d20fef9c95091c68f4e02f663bab427
Matt Rizzo <mattrizzo21@gmail.com>
2022-02-15 20:24:40 -0500

    added a data parser for the jsons

---
+++ src/data_parser.py
@@ -0,0 +1,30 @@
+import json
+import pathlib
+import os
+
+
+class DataParser():
+    def __init__(self) -> None:
+        self.default_auth_filename = "default_app_auth.json"
+        self.expected_auth_filename = "app_auth.json"
+
+        self.src_dir_path = pathlib.Path(__file__).parent.resolve()
+        self.root_dir_path = self.src_dir_path.parent.resolve()
+        self.path_to_data_dir = self.root_dir_path / "data"
+        self.default_auth_path = self.path_to_data_dir / self.default_auth_filename
+        self.expected_auth_path = self.path_to_data_dir / self.expected_auth_filename
+        if self._check_if_auth_file_exists():
+            pass
+
+    def _check_if_auth_file_exists(self) -> bool:
+        """Ensures the non-default auth file was created properly.
+        :return True if it exists"""
+        if not pathlib.Path(self.expected_auth_path).is_file():
+            print(f"Expected authorization data file not found at {self.expected_auth_path}")
+            print("Please see the /README.md for details about it's creation")
+            exit
+        else:
+            return True
+
+if __name__ == "__main__":
+    data_parser = DataParser()
