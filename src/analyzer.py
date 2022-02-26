c78babc9e0e2642eff571102e336e6d855abadc4
Matt Rizzo <mattrizzo21@gmail.com>
2022-02-26 16:38:45 -0500

    [refac] Further broke up analysis fun to make more readable, more modular, and easier to modify

--- src/analyzer.py
+++ src/analyzer.py
@@ -20,40 +20,22 @@ class Analyzer():
         analyzed_chart_data_artist = {}
         analyzed_chart_data_album = {}
 
-        # Used to remove ' from key names until it is safe to put them back in
-        # eventually the ' wrapped around key's are ALL changed to ",
-        # but that will also change ' in the keys to " which is BAD for parsing.
-        # change the ' to know what they are later
-        single_quote_escape_seq = constants.SINGLE_QUOTE_ESCAPE_SEQ
-        double_quote_escape_seq = constants.DOUBLE_QUOTE_ESCAPE_SEQ
-
         for raw_track in raw_track_list:
             processed_track = self.parse_raw_track(raw_track)
 
             cur_track = processed_track["track_name"]
-            cur_album = str(processed_track["album"])
-            cur_album = cur_album.replace("\'", single_quote_escape_seq).replace('\"', double_quote_escape_seq)
+
+            # Perform metric calcs for each artist and album - functions update in place
+            cur_artist = self._analyze_raw_track_artists(processed_track, analyzed_chart_data_artist)
+            cur_album = self._analyze_raw_track_album(processed_track, analyzed_chart_data_album)
 
             if cur_album == " " or cur_album == '':
                 cur_album = constants.DEFAULT_NO_ALBUM_NAME_MSG
 
-            cur_artist = str(processed_track["artists"][0])
-            cur_artist = cur_artist.replace("\'", single_quote_escape_seq).replace(
-                '\"', double_quote_escape_seq)
             if self._is_verbose:
                 print("Track {} by {} from their {} album".format(
                     cur_track, cur_artist, cur_album
                 ))
-
-            # do metric calc for each artist and album
-            # TODO: better handle features
-            cur_num_tracks_by_artist = analyzed_chart_data_artist.get(cur_artist, 0)
-            cur_num_tracks_by_artist += 1
-            analyzed_chart_data_artist[cur_artist] = cur_num_tracks_by_artist
-
-            cur_num_tracks_in_album = analyzed_chart_data_album.get(cur_album, 0)
-            cur_num_tracks_in_album += 1
-            analyzed_chart_data_album[cur_album] = cur_num_tracks_in_album
 
         return (analyzed_chart_data_artist, analyzed_chart_data_album)
 
@@ -75,3 +57,51 @@ class Analyzer():
             res_dict["artists"].append(artist_dict["name"])
 
         return res_dict
+
+    def _analyze_raw_track_artists(self, cur_parsed_track: Dict, cur_artist_info : Dict) -> str:
+        """:brief Given a track to analyze and the current analysis dict,\
+            updates `cur_artist_info` after analyzing the current track `IN PLACE`.
+        \n:param `cur_parsed_track` a dict representing a parsed track
+        \n:param `cur_artist_info` The current metrics on artists. `Will get updated in place`
+        :return The artist of the current track
+        """
+        # Used to remove ' from key names until it is safe to put them back in
+        # eventually the ' wrapped around key's are ALL changed to ",
+        # but that will also change ' in the keys to " which is BAD for parsing.
+        # change the ' to know what they are later
+        single_quote_escape_seq = constants.SINGLE_QUOTE_ESCAPE_SEQ
+        double_quote_escape_seq = constants.DOUBLE_QUOTE_ESCAPE_SEQ
+        cur_artist = cur_parsed_track["artists"][0]
+        cur_artist = cur_artist.replace("\'", single_quote_escape_seq).replace(
+            '\"', double_quote_escape_seq)
+
+        # do the metric calculations and update the results
+        # TODO: better handle features
+        cur_num_tracks_by_artist = cur_artist_info.get(cur_artist, 0)
+        cur_num_tracks_by_artist += 1
+        cur_artist_info[cur_artist] = cur_num_tracks_by_artist
+
+        return cur_artist
+
+    def _analyze_raw_track_album(self, cur_parsed_track: Dict, cur_album_info: Dict) -> str:
+        """:brief Given a track to analyze and the current analysis dict,\
+            updates `cur_album_info` after analyzing the current track `IN PLACE`.
+        \n:param `cur_parsed_track` a dict representing a parsed track
+        \n:param `cur_album_info` The current metrics on albums. `Will get updated in place`
+        \n:return The current album name
+        """
+        # Used to remove ' from key names until it is safe to put them back in
+        # eventually the ' wrapped around key's are ALL changed to ",
+        # but that will also change ' in the keys to " which is BAD for parsing.
+        # change the ' to know what they are later
+        single_quote_escape_seq = constants.SINGLE_QUOTE_ESCAPE_SEQ
+        double_quote_escape_seq = constants.DOUBLE_QUOTE_ESCAPE_SEQ
+        cur_album = str(cur_parsed_track["album"])
+        cur_album = cur_album.replace("\'", single_quote_escape_seq).replace('\"', double_quote_escape_seq)
+
+        cur_num_tracks_in_album = cur_album_info.get(cur_album, 0)
+        cur_num_tracks_in_album += 1
+        cur_album_info[cur_album] = cur_num_tracks_in_album
+
+        return cur_album
+
