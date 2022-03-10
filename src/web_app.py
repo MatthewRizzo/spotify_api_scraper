da8f3b2a43b26de3d9b58b6dbd6c1b02035c3606
Matt Rizzo <mattrizzo21@gmail.com>
2022-03-09 22:12:28 -0500

    remove unneeded line

--- src/web_app.py
+++ src/web_app.py
@@ -191,7 +191,6 @@ class WebApp(Scraper, UserManager, FlaskUtils):
 
     def create_response_uri_pages(self):
         """Used to make all routes REQUIRED by spotify to receive responses"""
-        @self._app.route("/redirect_after_auth", methods=["GET"], defaults={'code': None, 'state': None})
         @self._app.route("/redirect_after_auth?code=<code>&state=<state>", methods=["GET"])
         def redirect_after_auth():
             # part of this auth flow:
