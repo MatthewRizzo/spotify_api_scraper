21c8536ae97cb477c2f179b5af30b6c303e06aea
Matt Rizzo <mattrizzo21@gmail.com>
2022-03-09 21:44:52 -0500

    Can download a text file for the playlist songs

--- src/frontend/static/js/download-playlist-songs.js
+++ src/frontend/static/js/download-playlist-songs.js
@@ -3,11 +3,24 @@ $( document ).ready(function() {
     $("#download_songs_txt").click(download_songs);
 });
 
+/**
+ *
+ * @param {String} filename The name of the file - including its type
+ * @param {String} file_content What should get written to the file
+ */
+function make_download_file(filename, file_content)
+{
+    const blob = new Blob([file_content], {
+        type: "text/plain;charset=utf-8"
+    });
+
+    saveAs(blob, filename);
+}
 
 function download_songs()
 {
-    console.log("button press registered");
     const song_list_el = $("#song_list_display");
+    const playlist_name = $("#playlist_name").text();
 
     // seperate by new lines - each song
     let song_list = song_list_el.text().split(/\r?\n/);
@@ -15,11 +28,14 @@ function download_songs()
     // remove empty space songs
     song_list = song_list.filter(function(word) { return word.trim() != ''; })
 
-    // songs have "        " - artifact of jinja format. remove that
+    // remove any weird white space artifacts from jinja
     const final_song_list = song_list.map(song => song.trim());
-    console.log(`song_list = `)
-    for(const song of final_song_list)
-    {
-        console.log(song)
-    }
+
+    const file_content = final_song_list.join('\n');
+    let filename = playlist_name + ".txt";
+
+    // replace spaces with underscores
+    filename = filename.replace(/\s/g, "_");
+
+    make_download_file(filename, file_content);
 }
